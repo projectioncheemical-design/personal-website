7096bfdb77b5ae73a003d7264cff2f9348a9a4ec
Pro-cheem <elsiaad.motawee@gmail.com>
2025-09-28 15:33:14 +0300

    إعادة هيكلة: تنظيم ملفات التكوين في مجلد منفصل

---
+++ config/index.js
@@ -0,0 +1,7 @@
+// Export all configurations
+module.exports = {
+  next: require('./next.config'),
+  postcss: require('./postcss.config'),
+  tailwind: require('./tailwind.config'),
+  vercel: require('./vercel.json')
+};
